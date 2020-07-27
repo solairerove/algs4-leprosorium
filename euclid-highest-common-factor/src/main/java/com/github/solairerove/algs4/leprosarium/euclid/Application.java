a4baaffe43ffff54537fbf2ede74e091d0f27ad2
Mikita Kryvitski <solairerove@gmail.com>
2020-07-27 18:00:11 +0300

    feature: add simple maven project

---
+++ euclid-highest-common-factor/src/main/java/com/github/solairerove/algs4/leprosarium/euclid/Application.java
@@ -0,0 +1,7 @@
+package com.github.solairerove.algs4.leprosarium.euclid;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.printf("sosat %s", "oga");
+    }
+}
