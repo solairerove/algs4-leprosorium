9a352795cad377a02b2335e84a8ba1c5b72d4529
Mikita Kryvitski <solairerove@gmail.com>
2020-07-30 16:14:12 +0300

    feature: next exercises are done

--- exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
+++ exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
@@ -1,5 +1,7 @@
 package com.github.solairerove.algs4.leprosorium.exercise1;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("15 / 2 = %s\n", 15 / 2);
@@ -19,9 +21,51 @@ public class Main {
         System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 1.0, 2.0, inRangeOfZeroAndOne(1.0, 2.0));
         System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 0.5, 2.0, inRangeOfZeroAndOne(0.5, 2.0));
         System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 0.5, 0.1, inRangeOfZeroAndOne(0.5, 0.1));
+
+        System.out.println();
+        someShit();
+        someShit2();
+        someShit3();
+
+        System.out.println();
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+
+        System.out.printf("%s in binary string is %s\n", 5, Integer.toBinaryString(5));
+        System.out.printf("%s in binary string is %s\n", 1, Integer.toBinaryString(1));
+        System.out.printf("%s in binary string is %s\n", 0, Integer.toBinaryString(0));
     }
 
     private static boolean inRangeOfZeroAndOne(double a, double b) {
         return a < 1.0 && a > 0.0 && b < 1.0 && b > 0.0;
+    }
+
+    private static void someShit() {
+        double t = 9.0;
+        while (Math.abs(t - 9.0 / t) > .001) {
+            t = (9.0 / t + t) / 2.0;
+        }
+        StdOut.printf("%.5f\n", t);
+    }
+
+    private static void someShit2() {
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            for (int j = 0; j < i; j++) {
+                sum++;
+            }
+        }
+        StdOut.println(sum);
+    }
+
+    private static void someShit3() {
+        int sum = 0;
+        for (int i = 1; i < 1000; i *= 2) {
+            for (int j = 0; j < i; j++) {
+                sum++;
+            }
+        }
+        StdOut.println(sum);
     }
 }
