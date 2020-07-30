636f2323b614131703b5e6d29c7421b3c06fa674
Mikita Kryvitski <solairerove@gmail.com>
2020-07-30 15:17:53 +0300

    feature: write own fibonacci method

--- exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/StdlibExample.java
+++ exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/StdlibExample.java
@@ -6,6 +6,7 @@ public class StdlibExample {
     public static void main(String[] args) {
         System.out.printf("Fibonacci for n: %s \n", 15);
         fibonacci(15);
+        fibonacciMoreClear(15);
     }
 
     public static void fibonacci(int n) {
@@ -17,6 +18,20 @@ public class StdlibExample {
 
             a += b;
             b = a - b;
+        }
+        StdOut.println();
+    }
+
+    public static void fibonacciMoreClear(int n) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i <= n; ++i) {
+            StdOut.print(a + " ");
+
+            int temp = a;
+            a += b;
+            b = temp;
         }
         StdOut.println();
     }
