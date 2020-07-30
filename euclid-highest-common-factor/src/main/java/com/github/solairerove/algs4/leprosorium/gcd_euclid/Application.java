a47a70a362a6732ffbd6f9c25fe11d099bb4806f
Mikita Kryvitski <solairerove@gmail.com>
2020-07-30 18:19:57 +0300

    feature: small refactor of own gcd method

--- euclid-highest-common-factor/src/main/java/com/github/solairerove/algs4/leprosorium/gcd_euclid/Application.java
+++ euclid-highest-common-factor/src/main/java/com/github/solairerove/algs4/leprosorium/gcd_euclid/Application.java
@@ -2,10 +2,10 @@ package com.github.solairerove.algs4.leprosorium.gcd_euclid;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.printf("For 9 and 6 highest common factor is: %s\n", nonRecursiveNonInfiniteLoopGCD(9, 6)); // 3
-        System.out.printf("For 15 and 5 highest common factor is: %s\n", nonRecursiveNonInfiniteLoopGCD(15, 5)); // 5
-        System.out.printf("For 5 and 15 highest common factor is: %s\n", nonRecursiveNonInfiniteLoopGCD(5, 15)); // 5
-        System.out.printf("For 13 and 10 highest common factor is: %s\n", nonRecursiveNonInfiniteLoopGCD(13, 10)); // 1
+        System.out.printf("For 9 and 6 highest common factor is: %s\n", ownGcd(9, 6)); // 3
+        System.out.printf("For 15 and 5 highest common factor is: %s\n", ownGcd(15, 5)); // 5
+        System.out.printf("For 5 and 15 highest common factor is: %s\n", ownGcd(5, 15)); // 5
+        System.out.printf("For 13 and 10 highest common factor is: %s\n", ownGcd(13, 10)); // 1
     }
 
     // greatest common divisor
@@ -19,35 +19,13 @@ public class Application {
         return gcd(q, r);
     }
 
-    private static int nonRecursiveGCD(int p, int q) {
-        int first = p;
-        int second = q;
-
-        while (true) {
-            if (second == 0) {
-                return first;
-            }
-
-            int result = first % second;
-            first = second;
-            second = result;
-        }
-    }
-
-    private static int nonRecursiveNonInfiniteLoopGCD(int p, int q) {
-        int first = p;
-        int second = q;
-
-        int gcd = second;
-
-        while (second != 0) {
-            gcd = second;
-
-            int result = first % second;
-            first = second;
-            second = result;
+    public static int ownGcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
         }
 
-        return gcd;
+        return a;
     }
 }
