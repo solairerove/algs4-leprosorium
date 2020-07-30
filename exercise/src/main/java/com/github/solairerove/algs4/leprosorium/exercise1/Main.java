f0b2f147f474bcfedbf143d549d2e103faeb70f9
Mikita Kryvitski <solairerove@gmail.com>
2020-07-30 14:24:10 +0300

    feature: solve 1.1.5

--- exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
+++ exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
@@ -12,5 +12,16 @@ public class Main {
         System.out.printf("1 + 2 + 3 + 4.0 = %s\n", 1 + 2 + 3 + 4.0);
         System.out.printf("4.1 > 4 = %s\n", true);
         System.out.printf("1 + 2 + \"3\" = %s\n", 1 + 2 + "3");
+
+        System.out.println();
+
+        System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 1, 2, inRangeOfZeroAndOne(1, 2));
+        System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 1.0, 2.0, inRangeOfZeroAndOne(1.0, 2.0));
+        System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 0.5, 2.0, inRangeOfZeroAndOne(0.5, 2.0));
+        System.out.printf("a = %s, b = %s in range of 0 and 1 %s\n", 0.5, 0.1, inRangeOfZeroAndOne(0.5, 0.1));
+    }
+
+    private static boolean inRangeOfZeroAndOne(double a, double b) {
+        return a < 1.0 && a > 0.0 && b < 1.0 && b > 0.0;
     }
 }
