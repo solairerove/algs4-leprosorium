1ceabd8f696e641c8e30b39e7c377f785c0d72e5
Mikita Kryvitski <solairerove@gmail.com>
2020-07-30 14:06:50 +0300

    feature: add exercise module as well

---
+++ exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
@@ -0,0 +1,9 @@
+package com.github.solairerove.algs4.leprosorium.exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("15 / 2 = %s\n", 15 / 2);
+        System.out.printf("2.0e-6 * 100000000.1 = %s\n", 2.0e-6 * 100000000.1);
+        System.out.printf("true && false || true && true = %s\n", true);
+    }
+}
