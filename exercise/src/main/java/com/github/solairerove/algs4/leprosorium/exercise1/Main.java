d99eb662a0d15599fdf91705fd90fdc0e2499c6d
Mikita Kryvitski <solairerove@gmail.com>
2020-07-30 14:11:59 +0300

    feature: solve 1.1.2

--- exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
+++ exercise/src/main/java/com/github/solairerove/algs4/leprosorium/exercise1/Main.java
@@ -5,5 +5,12 @@ public class Main {
         System.out.printf("15 / 2 = %s\n", 15 / 2);
         System.out.printf("2.0e-6 * 100000000.1 = %s\n", 2.0e-6 * 100000000.1);
         System.out.printf("true && false || true && true = %s\n", true);
+
+        System.out.println();
+
+        System.out.printf("(1 + 2.236) / 2 = %s\n", (1 + 2.236) / 2);
+        System.out.printf("1 + 2 + 3 + 4.0 = %s\n", 1 + 2 + 3 + 4.0);
+        System.out.printf("4.1 > 4 = %s\n", true);
+        System.out.printf("1 + 2 + \"3\" = %s\n", 1 + 2 + "3");
     }
 }
