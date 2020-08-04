ca0d313a6d14ce9b696fab7756af8fe31e64d252
Mikita Kryvitski <solairerove@gmail.com>
2020-08-04 20:41:42 +0300

    feature: add linked stack implementation

---
+++ simple-collection/src/main/java/com/github/solairerove/algs4/leprosorium/simple_collection/linked/Stack.java
@@ -0,0 +1,43 @@
+package com.github.solairerove.algs4.leprosorium.simple_collection.linked;
+
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item> {
+
+    private Node first;
+    private int N;
+    private class Node{
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
