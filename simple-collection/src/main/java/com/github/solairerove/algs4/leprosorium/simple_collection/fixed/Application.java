f49e8fdce40eef5af040f95abb444a0cb7cbd78b
Mikita Kryvitski <solairerove@gmail.com>
2020-08-04 18:39:43 +0300

    feature: implement resizing array stack

--- simple-collection/src/main/java/com/github/solairerove/algs4/leprosorium/simple_collection/fixed/Application.java
+++ simple-collection/src/main/java/com/github/solairerove/algs4/leprosorium/simple_collection/fixed/Application.java
@@ -1,5 +1,6 @@
 package com.github.solairerove.algs4.leprosorium.simple_collection.fixed;
 
+import com.github.solairerove.algs4.leprosorium.simple_collection.resized.ResizingArrayStack;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -8,16 +9,21 @@ public class Application {
     // to be or not to - be - - that - - - is
     public static void main(String[] args) {
         FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(100);
+        ResizingArrayStack stack1 = new ResizingArrayStack();
+
         String s = new In("/Users/solairerove/Downloads/algs4-data/tobe.txt").readLine();
         String[] strings = s.split(" ");
         for (String item : strings) {
             if (!item.equals("-")) {
                 stack.push(item);
+                stack1.push(item);
             } else if (!stack.isEmpty()) {
-                StdOut.print(stack.pop() + " ");
+                StdOut.print(stack.pop() + " \n");
+                StdOut.print(stack1.pop() + " \n");
             }
         }
 
         System.out.printf("\nspace is: %s\n", stack.size());
+        System.out.printf("\nspace is: %s\n", stack1.size());
     }
 }
