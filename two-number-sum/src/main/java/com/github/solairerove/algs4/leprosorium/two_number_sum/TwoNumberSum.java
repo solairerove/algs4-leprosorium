21621c80f3ad7bfbf2378aa94739c3eb53d4a0b3
Mikita Kryvitski <solairerove@gmail.com>
2020-08-06 20:56:00 +0300

    doc: add arrays nested list

---
+++ two-number-sum/src/main/java/com/github/solairerove/algs4/leprosorium/two_number_sum/TwoNumberSum.java
@@ -0,0 +1,21 @@
+package com.github.solairerove.algs4.leprosorium.two_number_sum;
+
+/**
+ * Write a function that takes in a non-empty array of distinct integers and an integer
+ * representing a target sum. If any two numbers in the input array sum up to the
+ * target sum, the function should return them in an array, in any order. If no two
+ * numbers sum up tp the target sum, the function should return an empty array;
+ * <p>
+ * Known issues:
+ * - you can assume that there will be at most one pair of numbers summing up to the
+ * target sum
+ * <p>
+ * Sample Input
+ * array = [-1, 2, 3, 4, 5, -2, -3, 4]
+ * targetSum = -4
+ * <p>
+ * Sample Output
+ * [-3, -1]
+ */
+public class TwoNumberSum {
+}
