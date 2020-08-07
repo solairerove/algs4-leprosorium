432b6a66221c1353b88268fe080d0ba0950fb25c
Mikita Kryvitski <solairerove@gmail.com>
2020-08-07 11:27:49 +0300

    feature: add validate subsequence stub

--- validate-subsequence/src/main/java/com/github/solairerove/algs4/leprosorium/validate_subsequence/ValidateSubsequence.java
+++ validate-subsequence/src/main/java/com/github/solairerove/algs4/leprosorium/validate_subsequence/ValidateSubsequence.java
@@ -1,4 +1,22 @@
 package com.github.solairerove.algs4.leprosorium.validate_subsequence;
 
+import java.util.List;
+
+/**
+ * Given two non-empty arrays of integers, write a function that determines
+ * whether the second array is a subsequence of the first one.
+ * <p>
+ * Sample Input
+ * array = [5, 1, 22, 25, 6, -1, 8, 10]
+ * sequence = [1, 6, -1, 10]
+ */
 public class ValidateSubsequence {
+    public static void main(String... args) {
+
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        return false;
+    }
 }
