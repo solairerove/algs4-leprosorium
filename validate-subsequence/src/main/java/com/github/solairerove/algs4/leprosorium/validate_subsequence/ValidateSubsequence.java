a31ad2a7e607f3a735c61a236f66886cc0e92ec8
Mikita Kryvitski <solairerove@gmail.com>
2020-08-07 11:36:39 +0300

    feature: add non effective solution to validate subsequence problem

--- validate-subsequence/src/main/java/com/github/solairerove/algs4/leprosorium/validate_subsequence/ValidateSubsequence.java
+++ validate-subsequence/src/main/java/com/github/solairerove/algs4/leprosorium/validate_subsequence/ValidateSubsequence.java
@@ -12,11 +12,22 @@ import java.util.List;
  */
 public class ValidateSubsequence {
     public static void main(String... args) {
+        List<Integer> array = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = List.of(1, 6, -1, 10);
 
+        System.out.printf("Arrays is %s\n", array);
+        System.out.printf("Sequnce is %s\n", sequence);
+        System.out.printf("Is subsequence %s\n", isValidSubsequence(array, sequence));
     }
 
+    // O(N^2) time | O(1) space
     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-        // Write your code here.
-        return false;
+        for (Integer s : sequence) {
+            if (!array.contains(s)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
