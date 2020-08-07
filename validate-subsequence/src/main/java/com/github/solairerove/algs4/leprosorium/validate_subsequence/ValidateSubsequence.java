dda0191a7630c2272a4252290f08f64aca119d9a
Mikita Kryvitski <solairerove@gmail.com>
2020-08-07 11:46:06 +0300

    feature: add solution to validate subsequence problem

--- validate-subsequence/src/main/java/com/github/solairerove/algs4/leprosorium/validate_subsequence/ValidateSubsequence.java
+++ validate-subsequence/src/main/java/com/github/solairerove/algs4/leprosorium/validate_subsequence/ValidateSubsequence.java
@@ -6,6 +6,10 @@ import java.util.List;
  * Given two non-empty arrays of integers, write a function that determines
  * whether the second array is a subsequence of the first one.
  * <p>
+ * A subsequence of array is a set of numbers that aren't necessarily
+ * adjacent in the array but that are in the same order as they appear
+ * in the array.
+ * <p>
  * Sample Input
  * array = [5, 1, 22, 25, 6, -1, 8, 10]
  * sequence = [1, 6, -1, 10]
@@ -20,14 +24,18 @@ public class ValidateSubsequence {
         System.out.printf("Is subsequence %s\n", isValidSubsequence(array, sequence));
     }
 
-    // O(N^2) time | O(1) space
+    // O(N) time | O(1) space
     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-        for (Integer s : sequence) {
-            if (!array.contains(s)) {
-                return false;
+        int arrIdx = 0;
+        int seqIdx = 0;
+
+        while (arrIdx < array.size() && seqIdx < sequence.size()) {
+            if (array.get(arrIdx).equals(sequence.get(seqIdx))) {
+                seqIdx++;
             }
+            arrIdx++;
         }
 
-        return true;
+        return seqIdx == sequence.size();
     }
 }
