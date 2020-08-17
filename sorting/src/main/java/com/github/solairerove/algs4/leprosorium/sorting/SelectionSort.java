bb18de8fa9e3b95d5462dd586476f2c96edce6ad
Mikita Kryvitski <solairerove@gmail.com>
2020-08-17 22:11:16 +0300

    feature: fix fucking selection sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/SelectionSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/SelectionSort.java
@@ -3,6 +3,8 @@ package com.github.solairerove.algs4.leprosorium.sorting;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
     // O(n^2) time | O(1) space
@@ -15,8 +17,8 @@ public class SelectionSort {
                 if (less(a[j], a[min])) {
                     min = j;
                 }
-                swap(a, i, min);
             }
+            swap(a, i, min);
         }
     }
 
@@ -54,5 +56,28 @@ public class SelectionSort {
         show(array);
         sort(array);
         show(array);
+
+        int[] a = new int[]{8, 5, 2, 9, 5, 6, 3};
+        System.out.println(Arrays.toString(a));
+        selectionSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    private static void selectionSort(int[] a) {
+        int n = a.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+        }
     }
 }
