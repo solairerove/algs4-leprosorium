81501d462f1c631b206fe0241b571edb58587665
Mikita Kryvitski <solairerove@gmail.com>
2020-08-18 19:45:01 +0300

    feature: add bubble sort comparison

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/SortCompare.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/SortCompare.java
@@ -9,6 +9,7 @@ public class SortCompare {
         Stopwatch stopwatch = new Stopwatch();
         if ("Selection".equals(alg)) SelectionSort.sort(a);
         if ("Insertion".equals(alg)) InsertionSort.sort(a);
+        if ("Bubble".equals(alg)) BubbleSort.sort(a);
 
         return stopwatch.elapsedTime();
     }
@@ -31,13 +32,21 @@ public class SortCompare {
     public static void main(String[] args) {
         String alg1 = "Selection";
         String alg2 = "Insertion";
+        String alg3 = "Bubble";
         int N = 1000;
         int T = 100;
 
         double t1 = timeRandomInput(alg1, N, T);
         double t2 = timeRandomInput(alg2, N, T);
+        double t3 = timeRandomInput(alg3, N, T);
 
         StdOut.printf("For %d random Doubles\n %s in", N, alg1);
         StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
+
+        StdOut.printf("For %d random Doubles\n %s in", N, alg2);
+        StdOut.printf(" %.1f times faster than %s\n", t3 / t2, alg3);
+
+        StdOut.printf("For %d random Doubles\n %s in", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t3 / t1, alg3);
     }
 }
