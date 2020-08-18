6305d390f4fc711cdabc8083147eab39ad668420
Mikita Kryvitski <solairerove@gmail.com>
2020-08-18 18:37:52 +0300

    feature: add sort compare

---
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/SortCompare.java
@@ -0,0 +1,43 @@
+package com.github.solairerove.algs4.leprosorium.sorting;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class SortCompare {
+    public static double time(String alg, Double[] a) {
+        Stopwatch stopwatch = new Stopwatch();
+        if ("Selection".equals(alg)) SelectionSort.sort(a);
+        if ("Insertion".equals(alg)) InsertionSort.sort(a);
+
+        return stopwatch.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        double total = 0.0;
+
+        Double[] a = new Double[N];
+
+        for (int t = 0; t < T; t++) {
+            for (int i = 0; i < N; i++) {
+                a[i] = StdRandom.uniform();
+            }
+            total += time(alg, a);
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String alg1 = "Selection";
+        String alg2 = "Insertion";
+        int N = 1000;
+        int T = 100;
+
+        double t1 = timeRandomInput(alg1, N, T);
+        double t2 = timeRandomInput(alg2, N, T);
+
+        StdOut.printf("For %d random Doubles\n %s in", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
+    }
+}
