6aa3350fa0769009b4e19946be2f9b81e44b3e49
Mikita Kryvitski <solairerove@gmail.com>
2020-08-18 19:32:42 +0300

    feature: add one more bubble sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/BubbleSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/BubbleSort.java
@@ -3,6 +3,8 @@ package com.github.solairerove.algs4.leprosorium.sorting;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
     public static void sort(Comparable[] a) {
@@ -51,5 +53,31 @@ public class BubbleSort {
         show(array);
         sort(array);
         show(array);
+
+        int[] a = new int[]{8, 5, 2, 9, 5, 6, 3};
+        System.out.println(Arrays.toString(a));
+        bubbleSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    private static void bubbleSort(int[] a) {
+        int n = a.length;
+
+        int cnt = 0;
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 0; i < n - 1 - cnt; i++) {
+                if (a[i + 1] < a[i]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+
+                    isSorted = false;
+                }
+            }
+            cnt++;
+        }
     }
 }
