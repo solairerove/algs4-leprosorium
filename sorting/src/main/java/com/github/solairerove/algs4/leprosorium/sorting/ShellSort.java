2f7eecd820858c6dcb554472a394528cacf8d5a7
Mikita Kryvitski <solairerove@gmail.com>
2020-08-19 20:49:42 +0300

    feature: add shell sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/ShellSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/ShellSort.java
@@ -7,7 +7,17 @@ import edu.princeton.cs.algs4.StdOut;
 public class ShellSort {
 
     public static void sort(Comparable[] a) {
-
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3) h = 3 * h + 1;
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    swap(a, j, j - h);
+                }
+            }
+            h /= 3;
+        }
     }
 
     private static boolean less(Comparable v, Comparable w) {
