204ec11a277abee83383adfa9adff2d06c23e093
Mikita Kryvitski <solairerove@gmail.com>
2020-08-20 21:32:39 +0300

    feature: add merge sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/MergeSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/MergeSort.java
@@ -3,6 +3,8 @@ package com.github.solairerove.algs4.leprosorium.sorting;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     private static Comparable[] aux;
@@ -72,5 +74,44 @@ public class MergeSort {
         show(array);
         sort(array);
         show(array);
+
+        int[] a = new int[]{8, 5, 2, 9, 5, 6, 3};
+        System.out.println(Arrays.toString(a));
+        sortTD(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    private static void sortTD(int[] a) {
+        int n = a.length;
+
+        int[] aux = new int[n];
+        sortTD(a, aux, 0, n - 1);
+    }
+
+    private static void sortTD(int[] a, int[] aux, int low, int high) {
+        if (high <= low) {
+            return;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        sortTD(a, aux, low, mid);
+        sortTD(a, aux, mid + 1, high);
+        merge(a, aux, low, mid, high);
+    }
+
+    private static void merge(int[] a, int[] aux, int low, int mid, int high) {
+        int i = low, j = mid + 1;
+
+        for (int k = low; k <= high; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = low; k <= high; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > high) a[k] = aux[i++];
+            else if (aux[j] < aux[i]) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
     }
 }
