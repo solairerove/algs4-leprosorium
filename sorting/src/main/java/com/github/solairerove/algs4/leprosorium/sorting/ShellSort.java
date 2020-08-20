0d834be133cf5c2c8f686937a33e4477fd13cb26
Mikita Kryvitski <solairerove@gmail.com>
2020-08-20 09:07:37 +0300

    feature: add void shell sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/ShellSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/ShellSort.java
@@ -3,6 +3,8 @@ package com.github.solairerove.algs4.leprosorium.sorting;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 // insertion steroid
 public class ShellSort {
 
@@ -54,5 +56,31 @@ public class ShellSort {
         show(array);
         sort(array);
         show(array);
+
+        int[] a = new int[]{8, 5, 2, 9, 5, 6, 3};
+        System.out.println(Arrays.toString(a));
+        shellSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void shellSort(int[] a) {
+        int n = a.length;
+        int h = 1;
+
+        while (h < n / 3) h = h * 3 + 1;
+        while (h >= 1) {
+            for (int i = h; i < n; i++) {
+                for (int j = i; j >= h && a[j] < a[j - h]; j -= h) {
+                    swap(a, j, j - h);
+                }
+            }
+            h /= 3;
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
