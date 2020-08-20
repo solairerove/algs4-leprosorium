4573b8d68ea4225f1f4f35599b199ab6d732d2ed
Mikita Kryvitski <solairerove@gmail.com>
2020-08-20 21:01:16 +0300

    feature: add merge sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/MergeSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/MergeSort.java
@@ -5,8 +5,37 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class MergeSort {
 
-    public static void sort(Comparable[] a) {
+    private static Comparable[] aux;
 
+    // O(nlog(n)) time | O(n) space
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int low, int high) {
+        if (high <= low) return;
+
+        int mid = low + (high - low) / 2;
+        sort(a, low, mid);
+        sort(a, mid + 1, high);
+        merge(a, low, mid, high);
+    }
+
+    private static void merge(Comparable[] a, int low, int mid, int high) {
+        // merge a[low..mid] with a[mid+1..high]
+        int i = low, j = mid + 1;
+
+        for (int k = low; k <= high; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = low; k <= high; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > high) a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
     }
 
     private static boolean less(Comparable v, Comparable w) {
