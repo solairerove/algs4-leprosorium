0d53375ca924d02fec9fb3ad9cd1e81ec8615dce
Mikita Kryvitski <solairerove@gmail.com>
2020-08-21 19:08:46 +0300

    feature: add quick sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/QuickSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/QuickSort.java
@@ -6,7 +6,28 @@ import edu.princeton.cs.algs4.StdOut;
 public class QuickSort {
 
     public static void sort(Comparable[] a) {
+        sort(a, 0, a.length - 1);
+    }
 
+    public static void sort(Comparable[] a, int low, int high) {
+        if (high <= low) return; // if (high <= low + M (5-15)) { Insertion.sort(a, low, high); return }
+        int j = partition(a, low, high);
+        sort(a, low, j - 1);
+        sort(a, j + 1, high);
+    }
+
+    private static int partition(Comparable[] a, int low, int high) {
+        int i = low, j = high + 1;
+        Comparable v = a[low];
+
+        while (true) {
+            while (less(a[++i], v)) if (i == high) break;
+            while (less(v, a[--j])) if (j == low) break;
+            if (i >= j) break;
+            swap(a, i, j);
+        }
+        swap(a, low, j); // v помещает на свое мето
+        return j;
     }
 
     private static boolean less(Comparable v, Comparable w) {
