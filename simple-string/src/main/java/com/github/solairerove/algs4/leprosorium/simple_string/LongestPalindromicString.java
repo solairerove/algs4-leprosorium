84ebac4af1b57feed3b1a809d9d23f5a8b67e8af
Mikita Kryvitski <solairerove@gmail.com>
2020-08-24 21:40:58 +0300

    feature: fix the longest palindrome problem solution

--- simple-string/src/main/java/com/github/solairerove/algs4/leprosorium/simple_string/LongestPalindromicString.java
+++ simple-string/src/main/java/com/github/solairerove/algs4/leprosorium/simple_string/LongestPalindromicString.java
@@ -31,7 +31,7 @@ public class LongestPalindromicString {
 
     private static boolean isPalindrome(String s) {
         int n = s.length();
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n / 2; i++)
             if (s.charAt(i) != s.charAt(n - 1 - i)) return false;
         return true;
     }
