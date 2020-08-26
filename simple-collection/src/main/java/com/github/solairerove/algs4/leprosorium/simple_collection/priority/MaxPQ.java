7232d1d38cfb70f0ffb8ce991b0ae6f257fb74f8
Mikita Kryvitski <solairerove@gmail.com>
2020-08-26 19:41:00 +0300

    feature: add priority queue private help methods

--- simple-collection/src/main/java/com/github/solairerove/algs4/leprosorium/simple_collection/priority/MaxPQ.java
+++ simple-collection/src/main/java/com/github/solairerove/algs4/leprosorium/simple_collection/priority/MaxPQ.java
@@ -2,6 +2,9 @@ package com.github.solairerove.algs4.leprosorium.simple_collection.priority;
 
 public class MaxPQ<Key extends Comparable<Key>> {
 
+    private Key[] pq;
+    private int N;
+
     public MaxPQ() {
 
     }
@@ -32,5 +35,32 @@ public class MaxPQ<Key extends Comparable<Key>> {
 
     int size() {
         return 0;
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void swap(int i, int j) {
+        var temp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = temp;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            swap(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) j++;
+            if (!less(k, j)) break;
+            swap(k, j);
+            k = j;
+        }
     }
 }
