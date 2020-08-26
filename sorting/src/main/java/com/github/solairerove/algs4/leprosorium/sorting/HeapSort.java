b7907f11f19f06340eefee67f241cbf339996d66
Mikita Kryvitski <solairerove@gmail.com>
2020-08-26 20:35:46 +0300

    feature: add heap sort

--- sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/HeapSort.java
+++ sorting/src/main/java/com/github/solairerove/algs4/leprosorium/sorting/HeapSort.java
@@ -6,17 +6,36 @@ import edu.princeton.cs.algs4.StdOut;
 public class HeapSort {
 
     public static void sort(Comparable[] a) {
+        int n = a.length;
 
+        for (int k = n / 2; k >= 1; k--) {
+            sink(a, k, n);
+        }
+        int k = n;
+        while (k > 1) {
+            swap(a, 1, k--);
+            sink(a, 1, k);
+        }
     }
 
-    private static boolean less(Comparable v, Comparable w) {
-        return v.compareTo(w) < 0;
+    private static void sink(Comparable[] a, int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(a, j, j + 1)) j++;
+            if (!less(a, k, j)) break;
+            swap(a, k, j);
+            k = j;
+        }
+    }
+
+    private static boolean less(Comparable[] a, int i, int j) {
+        return a[i - 1].compareTo(a[j - 1]) < 0;
     }
 
     private static void swap(Comparable[] a, int i, int j) {
-        Comparable t = a[i];
-        a[i] = a[j];
-        a[j] = t;
+        Comparable t = a[i - 1];
+        a[i - 1] = a[j - 1];
+        a[j - 1] = t;
     }
 
     private static void show(Comparable[] a) {
@@ -24,16 +43,6 @@ public class HeapSort {
             StdOut.print(el + " ");
         }
         StdOut.println();
-    }
-
-    private static boolean isSorted(Comparable[] a) {
-        for (int i = 1; i < a.length; ++i) {
-            if (less(a[i], a[i - 1])) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     public static void main(String... args) {
