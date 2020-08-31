9a63740a87ded8e8f2443030e6d368145d189ba7
Mikita Kryvitski <solairerove@gmail.com>
2020-08-31 17:57:59 +0300

    doc: add the longest deduplicate string

---
+++ simple-string/src/main/java/com/github/solairerove/algs4/leprosorium/simple_string/LongestDeduplicateSubstring.java
@@ -0,0 +1,12 @@
+package com.github.solairerove.algs4.leprosorium.simple_string;
+
+public class LongestDeduplicateSubstring {
+    public static void main(String[] args) {
+        String s = "nikitaloxpizdec"; // taloxpizdec
+        System.out.printf("%s %s", s, getTheLongestDeduplicateSubstring(s));
+    }
+
+    private static String getTheLongestDeduplicateSubstring(String s) {
+        return s;
+    }
+}
