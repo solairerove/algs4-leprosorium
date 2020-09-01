27a84b607ab6ccdb1630d953f2348916c2e49c46
Mikita Kryvitski <solairerove@gmail.com>
2020-09-01 18:45:11 +0300

    feature: implement delete method in seq search st

--- symbol-table/src/main/java/com/github/solairerove/algs4/leprosorium/symbol_table/Application.java
+++ symbol-table/src/main/java/com/github/solairerove/algs4/leprosorium/symbol_table/Application.java
@@ -4,9 +4,13 @@ public class Application {
     public static void main(String... args) {
         var map = new SequentialSearchST<String, String>();
         map.put("lox", "pidor");
-        map.put("no", "friends");
+        map.put("no", "friends"); // will be first
+        map.put("nora", "noir"); // delete
 
-        // O(n)
-        System.out.println(map.get("no")); //friends
+        System.out.println(map.get("no")); // friends
+        System.out.println(map.contains("no")); // true
+        System.out.println(map.contains("false")); // false
+        System.out.println(map.isEmpty()); // false
+        System.out.println(map.size()); // 3
     }
 }
