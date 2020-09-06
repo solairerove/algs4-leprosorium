0fb3aae1a1a35d6b351cb1d99b32715ff2164233
Mikita Kryvitski <solairerove@gmail.com>
2020-09-06 16:20:56 +0300

    feature: add delete method

--- binary-search-tree/src/main/java/com/github/solairerove/algs4/leprosorium/binary_search_tree/BST.java
+++ binary-search-tree/src/main/java/com/github/solairerove/algs4/leprosorium/binary_search_tree/BST.java
@@ -127,4 +127,26 @@ public class BST<Key extends Comparable<Key>, Value> {
         x.n = size(x.left) + size(x.right) + 1;
         return x;
     }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.n = size(x.left) + size(x.right) + 1;
+        return x;
+    }
 }
