fc2e62dc1229c637cc354a58260f062496dea92a
Mikita Kryvitski <solairerove@gmail.com>
2020-09-06 15:18:17 +0300

    feature: just small refactor

--- binary-search-tree/src/main/java/com/github/solairerove/algs4/leprosorium/binary_search_tree/BST.java
+++ binary-search-tree/src/main/java/com/github/solairerove/algs4/leprosorium/binary_search_tree/BST.java
@@ -102,4 +102,17 @@ public class BST<Key extends Comparable<Key>, Value> {
         else if (t < k) return select(x.right, k - t - 1);
         else return x;
     }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) return 0;
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(key, x.left);
+        else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+        else return size(x.left);
+    }
 }
