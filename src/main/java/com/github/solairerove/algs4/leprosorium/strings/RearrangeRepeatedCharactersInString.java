38a96d095e7b32c5fb7a1a9a497d9a956e07200b
Mikita Kryvitski <solairerove@gmail.com>
2020-09-10 18:50:39 +0300

    feature: add rearrange repeated chars isNone check

--- src/main/java/com/github/solairerove/algs4/leprosorium/strings/RearrangeRepeatedCharactersInString.java
+++ src/main/java/com/github/solairerove/algs4/leprosorium/strings/RearrangeRepeatedCharactersInString.java
@@ -1,5 +1,8 @@
 package com.github.solairerove.algs4.leprosorium.strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Given a string with repeated characters, rearrange the
  string so that no two adjacent characters are the same.
@@ -14,5 +17,37 @@ public class RearrangeRepeatedCharactersInString {
         String s3 = "aaabbbbb"; // None
         String s4 = "caaabbbbbb"; // None
         String s5 = "aaaaabbc"; // None
+
+        System.out.println(isNone(getMap(s1), s1.length()));
+        System.out.println(isNone(getMap(s2), s2.length()));
+        System.out.println(isNone(getMap(s3), s3.length()));
+        System.out.println(isNone(getMap(s4), s4.length()));
+        System.out.println(isNone(getMap(s5), s5.length()));
+    }
+
+    private static boolean isNone(Map<Character, Integer> map, int length) {
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            var current = entry.getValue();
+            if (current > length - current) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Map<Character, Integer> getMap(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (map.containsKey(current)) {
+                map.put(current, map.get(current) + 1);
+            } else {
+                map.put(current, 1);
+            }
+        }
+
+        return map;
     }
 }
