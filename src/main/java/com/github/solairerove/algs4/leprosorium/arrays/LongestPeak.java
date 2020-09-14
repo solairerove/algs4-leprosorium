d6317e962f5b5e6a471fcff28ccd738b0982ac1d
Mikita Kryvitski <solairerove@gmail.com>
2020-09-14 22:21:36 +0300

    doc: add long peak template

---
+++ src/main/java/com/github/solairerove/algs4/leprosorium/arrays/LongestPeak.java
@@ -0,0 +1,10 @@
+package com.github.solairerove.algs4.leprosorium.arrays;
+
+/**
+ * Find length of longest peak(adjacent increasing elements) in array.
+ */
+public class LongestPeak {
+    public static void main(String[] args) {
+        int[] a = {0, 1, 2, 3, 4, 4, -1, 12, 10, 6, 5, 4, 3, 2, -1, -3, 2, 4};
+    }
+}
