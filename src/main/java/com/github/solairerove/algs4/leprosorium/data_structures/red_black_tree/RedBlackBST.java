bfc262b71e8547778ca8a1684e0312f3354425ef
Mikita Kryvitski <solairerove@gmail.com>
2020-09-16 19:30:56 +0300

    feature: add standard bst get method and javadocs

--- src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/red_black_tree/RedBlackBST.java
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/red_black_tree/RedBlackBST.java
@@ -21,25 +21,94 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         }
     }
 
-    public int size() {
-        return size(root);
-    }
+    /***************************************************************************
+     *  Node helper methods.
+     ***************************************************************************/
 
-    private int size(Node x) {
-        if (x == null) return 0;
-        else return x.n;
-    }
-
+    // is node x red; false if x is null ?
     private boolean isRed(Node x) {
         if (x == null) return false;
         return RED == x.color;
     }
 
+    /**
+     * Is this symbol table empty?
+     *
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return size(root);
+    }
+
+    // number of node in subtree rooted at x; 0 if x is null
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.n;
+    }
+
+    /***************************************************************************
+     *  Standard BST search.
+     ***************************************************************************/
+
+    // value associated with the given key in subtree rooted at x; null if no such key
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.value;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     * and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        return get(root, key);
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     * {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    /***************************************************************************
+     *  Red-black tree helper functions.
+     ***************************************************************************/
+
+    // make a right-leaning link lean to the left
     private Node rotateLeft(Node h) {
         Node x = h.right;
         h.right = x.left;
         x.left = h;
 
+        // x.color = x.left.color;
+        // x.left.color = RED;
         x.color = h.color;
         h.color = RED;
 
@@ -48,11 +117,14 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    // make a left-leaning link lean to the right
     private Node rotateRight(Node h) {
         Node x = h.left;
         h.left = x.right;
         x.right = h;
 
+        // x.color = x.right.color;
+        // x.right.color = RED;
         x.color = h.color;
         h.color = RED;
 
