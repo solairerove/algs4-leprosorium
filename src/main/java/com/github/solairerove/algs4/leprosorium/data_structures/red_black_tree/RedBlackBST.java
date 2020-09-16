90e3626c1ec197a619514688b789e0693e6a6aad
Mikita Kryvitski <solairerove@gmail.com>
2020-09-16 19:18:14 +0300

    feature: add red black helper methods

--- src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/red_black_tree/RedBlackBST.java
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/red_black_tree/RedBlackBST.java
@@ -1,4 +1,63 @@
 package com.github.solairerove.algs4.leprosorium.data_structures.red_black_tree;
 
-public class RedBlackBST {
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left, right;
+        private boolean color;
+        private int n;
+
+        public Node(Key key, Value value, boolean color, int n) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+            this.n = n;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.n;
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return RED == x.color;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.n = h.n;
+        h.n = 1 + size(h.left) + size(x.right);
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        x.n = h.n;
+        h.n = 1 + size(h.left) + size(h.right);
+        return x;
+    }
 }
