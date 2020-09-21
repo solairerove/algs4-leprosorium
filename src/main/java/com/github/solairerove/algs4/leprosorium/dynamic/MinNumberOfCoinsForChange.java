44572a39434662f2f9cff21849a7add174fadee2
Mikita Kryvitski <solairerove@gmail.com>
2020-09-21 19:57:19 +0300

    doc: min number of coins for change

---
+++ src/main/java/com/github/solairerove/algs4/leprosorium/dynamic/MinNumberOfCoinsForChange.java
@@ -0,0 +1,19 @@
+package com.github.solairerove.algs4.leprosorium.dynamic;
+
+/**
+ * given array as coin denominations and n as target amount
+ * return smallest number of coins to make change.
+ * n = 6
+ * [1, 2, 5]
+ * 2 -> 1x1 + 5x1
+ */
+public class MinNumberOfCoinsForChange {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 5};
+        int n = 6;
+    }
+
+    private static int minNumOfCoins(int[] a, int n) {
+        return -1;
+    }
+}
