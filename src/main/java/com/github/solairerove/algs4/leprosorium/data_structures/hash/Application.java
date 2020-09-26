49cfd9861ec51b8ecf55e50f54afeaee6573414e
Mikita Kryvitski <solairerove@gmail.com>
2020-09-26 11:30:29 +0300

    feature: implement separate chaining hash st

--- src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/hash/Application.java
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/hash/Application.java
@@ -1,4 +1,17 @@
 package com.github.solairerove.algs4.leprosorium.data_structures.hash;
 
 public class Application {
+    public static void main(String[] args) {
+        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<>();
+        st.put("X", 0);
+        st.put("S", 1);
+        st.put("R", 2);
+        st.put("M", 3);
+        st.put("H", 4);
+        st.put("E", 5);
+        st.put("C", 6);
+        st.put("A", 11);
+
+        System.out.println(st.get("X")); // 0
+    }
 }
--- src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/hash/SeparateChainingHashST.java
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/hash/SeparateChainingHashST.java
@@ -1,4 +1,58 @@
 package com.github.solairerove.algs4.leprosorium.data_structures.hash;
 
-public class SeparateChainingHashST {
+import com.github.solairerove.algs4.leprosorium.data_structures.symbol_table.sequential.SequentialSearchST;
+
+public class SeparateChainingHashST<Key, Value> {
+    private static final int INIT_CAPACITY = 4;
+
+    private int n; // number of key-value pairs
+    private int m; // hash table size
+    private SequentialSearchST<Key, Value>[] st; // array of linked list tables
+
+    public SeparateChainingHashST() {
+        this(INIT_CAPACITY);
+    }
+
+    public SeparateChainingHashST(int m) {
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++) st[i] = new SequentialSearchST<>();
+    }
+
+    private void resize(int chains) {
+
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        int i = hash(key);
+        return st[i].get(key);
+    }
+
+    public void put(Key key, Value value) {
+        if (key == null) throw new IllegalArgumentException("key argument to put() is null");
+
+        if (n >= 10 * m) resize(2 * m);
+
+        int i = hash(key);
+        if (!st[i].contains(key)) n++;
+        st[i].put(key, value);
+    }
 }
