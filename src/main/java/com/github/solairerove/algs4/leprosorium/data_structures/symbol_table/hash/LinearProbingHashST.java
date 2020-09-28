efb2bf9935d774de07f51eb846fdef1d10bf08aa
Mikita Kryvitski <solairerove@gmail.com>
2020-09-28 22:44:17 +0300

    feature: linear probing main api methods

--- src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/symbol_table/hash/LinearProbingHashST.java
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/symbol_table/hash/LinearProbingHashST.java
@@ -1,4 +1,70 @@
 package com.github.solairerove.algs4.leprosorium.data_structures.symbol_table.hash;
 
-public class LinearProbingHashST {
+public class LinearProbingHashST<Key, Value> {
+    private static final int INIT_CAPACITY = 4;
+
+    private int n; // number of key-value pairs
+    private int m; // size of linear probing table
+    private Key[] keys;
+    private Value[] values;
+
+    public LinearProbingHashST() {
+        this(INIT_CAPACITY);
+    }
+
+    public LinearProbingHashST(int capacity) {
+        this.m = capacity;
+        n = 0;
+        keys = (Key[]) new Object[m];
+        values = (Value[]) new Object[m];
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
+    }
+
+    public void put(Key key, Value value) {
+        if (key == null) throw new IllegalArgumentException("argument to put() is null");
+
+//        if (value == null) {
+//            delete(key);
+//            return;
+//        }
+
+//        if (n >= m / 2) resize(2 * m);
+
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[i] = key;
+        values[i] = value;
+        n++;
+    }
 }
