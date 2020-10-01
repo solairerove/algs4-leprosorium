cad5e981b01fd9a49a7ebb1a51c35c22285590eb
Mikita Kryvitski <solairerove@gmail.com>
2020-10-01 14:06:43 +0300

    doc: add sparse vector template

---
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/symbol_table/page_rank/Application.java
@@ -0,0 +1,7 @@
+package com.github.solairerove.algs4.leprosorium.data_structures.symbol_table.page_rank;
+
+public class Application {
+    public static void main(String... args) {
+
+    }
+}
---
+++ src/main/java/com/github/solairerove/algs4/leprosorium/data_structures/symbol_table/page_rank/SparseVector.java
@@ -0,0 +1,4 @@
+package com.github.solairerove.algs4.leprosorium.data_structures.symbol_table.page_rank;
+
+public class SparseVector {
+}
