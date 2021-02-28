bc0b7571dd2f6750530ad30b5f266ad66f401929
Mikita Kryvitski <solairerove@gmail.com>
2021-02-28 18:01:26 +0300

    feature: add recursive version of binary search

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/classic/BinarySearch.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/classic/BinarySearch.kt
@@ -6,11 +6,12 @@ fun main() {
 
     val sorted = items.sorted()
     print("items: $items \n")
-    print("trying to fine 10, index: ${binarySearch(sorted, 10)} \n") // 8
-    print("trying to fine 10, index: ${binarySearch(sorted, 6)} \n") // 4
-    print("trying to fine 10, index: ${binarySearch(sorted, 0)} \n") // -1
+    print("trying to fine 10, index: ${binarySearch(sorted, 10)} - ${rank(sorted, 10)} \n") // 8
+    print("trying to fine 10, index: ${binarySearch(sorted, 6)} - ${rank(sorted, 6)} \n") // 4
+    print("trying to fine 10, index: ${binarySearch(sorted, 0)} - ${rank(sorted, 0)} \n") // -1
 }
 
+// O(log(n)) time | O(1) space
 private fun binarySearch(items: List<Int>, item: Int): Int {
     var low = 0
     var high = items.size - 1
@@ -27,4 +28,22 @@ private fun binarySearch(items: List<Int>, item: Int): Int {
     }
 
     return -1
+}
+
+// O(log(n)) time | O(log(n)) space cause of stack call
+private fun rank(items: List<Int>, item: Int): Int {
+    return rank(items, item, 0, items.size)
+}
+
+private fun rank(items: List<Int>, item: Int, low: Int, high: Int): Int {
+    if (low > high) return -1
+
+    val mid = (low + high) / 2
+    val guess = items[mid]
+
+    return when {
+        guess == item -> mid
+        guess > item -> rank(items, item, low, mid - 1)
+        else -> rank(items, item, mid + 1, high)
+    }
 }
