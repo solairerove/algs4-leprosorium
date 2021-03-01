4c874fec9e716a594504b8e3bf8ff3d3e624423e
Mikita Kryvitski <solairerove@gmail.com>
2021-03-01 14:31:47 +0300

    feature: add bubble sort

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/sorting/BubbleSort.kt
@@ -0,0 +1,27 @@
+package com.github.solairerove.algs4.leprosorium.sorting
+
+fun main() {
+    val items = mutableListOf(5, 6, 4, 3, 10, 9, 5, 6, 7)
+    print("items: $items \n")
+    bubbleSort(items)
+    print("items: $items \n")
+}
+
+// O(n^2) time | O(1) space
+private fun bubbleSort(arr: MutableList<Int>) {
+    val n = arr.size
+
+    for (i in 0 until n) {
+        for (j in 0 until (n - i - 1)) {
+            if (arr[j + 1] < arr[j]) {
+                swap(arr, j + 1, j)
+            }
+        }
+    }
+}
+
+private fun swap(arr: MutableList<Int>, i: Int, j: Int) {
+    val temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
