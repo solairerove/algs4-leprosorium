06f0a3da169f9ac563da7847d10af967b6e07187
Mikita Kryvitski <solairerove@gmail.com>
2021-03-01 21:26:49 +0300

    feature: add pisano period

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/recursion/Fibonacci.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/recursion/Fibonacci.kt
@@ -1,5 +1,7 @@
 package com.github.solairerove.algs4.leprosorium.recursion
 
+import java.math.BigInteger
+
 fun main() {
     for (i in 1..6) {
         print("${getNthFibonacci(i)} ")
@@ -12,6 +14,13 @@ fun main() {
     }
 
     print("${getLastNumberOfNthFibonacci(841645)} ") // 5
+
+    print("\n")
+
+    val n = BigInteger.valueOf(12589)
+    val m = BigInteger.valueOf(369)
+    val round = getModOfBigNthFibonacci(n, m)
+    print("${round[n.mod(BigInteger.valueOf(round.size.toLong())).intValueExact()]} ") // 89
 }
 
 // O(n) time | O(1) space
@@ -38,4 +47,31 @@ private fun getLastNumberOfNthFibonacci(n: Int): Int {
     }
 
     return if (n > 1) lastTwo[1] else lastTwo[0]
+}
+
+// TODO: move to PisanoPeriod
+// 1 <= n <= 10^18 and 2 <= m <= 10^5 Pisano period
+private fun getModOfBigNthFibonacci(n: BigInteger, m: BigInteger): List<BigInteger> {
+    val round = mutableListOf(BigInteger.ZERO, BigInteger.ONE)
+
+    var cnt = BigInteger.ONE
+
+    while (cnt < n) {
+        val next = round[cnt.intValueExact()]
+            .add(round[cnt.subtract(BigInteger.ONE).intValueExact()])
+            .mod(m)
+
+        if (round[round.size - 1].compareTo(BigInteger.ZERO) == 0
+            && next.compareTo(BigInteger.ONE) == 0
+        ) {
+            round.removeAt(round.size - 1)
+            break
+        }
+
+        round.add(next)
+
+        cnt = cnt.add(BigInteger.ONE)
+    }
+
+    return round
 }
