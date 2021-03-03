584804607c39970869f0be754858bcb0b47c00a4
Mikita Kryvitski <solairerove@gmail.com>
2021-03-03 16:53:41 +0300

    feature: add palindrome check

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/strings/PalindromeCheck.kt
@@ -0,0 +1,19 @@
+package com.github.solairerove.algs4.leprosorium.strings
+
+fun main() {
+    print("'string' is palindrome ${isPalindrome("string")}\n")
+    print("'aba' is palindrome ${isPalindrome("aba")}\n")
+    print("'sobaka' is palindrome ${isPalindrome("sobaka")}\n")
+    print("'civic' is palindrome ${isPalindrome("civic")}\n")
+    print("'level' is palindrome ${isPalindrome("level")}\n")
+    print("'racecar' is palindrome ${isPalindrome("racecar")}\n")
+}
+
+private fun isPalindrome(s: String): Boolean {
+    for (i in s.indices) {
+        if (s[i] != s[s.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
