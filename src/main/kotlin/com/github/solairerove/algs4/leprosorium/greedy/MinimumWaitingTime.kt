34055e6a62ea2894f50c89a8fb346112b66726a1
Mikita Kryvitski <solairerove@gmail.com>
2021-03-08 16:44:51 +0300

    feature: add greedy minimal waiting time

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/greedy/MinimumWaitingTime.kt
@@ -0,0 +1,25 @@
+package com.github.solairerove.algs4.leprosorium.greedy
+
+/**
+ * Given non empty array of positive int, that define time to execute.
+ * Only one query can be executed at time, but in any order.
+ * Get minimal waiting time to execute all of queries.
+ * 9 6 5 2 3 1 -> 1 2 3 5 6 9 -> 0 + 1 + (1 + 2) + (1 + 2 + 3) + (1 + 2 + 3 + 5) + (1 + 2 + 3 + 5 + 6) = 38
+ */
+fun main() {
+    print(minimumWaitingTime(mutableListOf(9, 6, 5, 2, 3, 1)))
+}
+
+// O(nlogn) time | O(1) space
+private fun minimumWaitingTime(queries: MutableList<Int>): Int {
+    queries.sort()
+
+    var minimumWaitingTime = 0
+    var previousSum = 0
+    for (i in 1..queries.size) {
+        minimumWaitingTime += previousSum
+        previousSum += queries[i - 1]
+    }
+
+    return minimumWaitingTime
+}
