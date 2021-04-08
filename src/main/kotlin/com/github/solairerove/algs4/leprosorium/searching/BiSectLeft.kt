4e6f0e1081f5353217f284abfa46186903a9b6a2
Mikita Kryvitski <solairerove@gmail.com>
2021-04-08 22:22:48 +0300

    feature: bisect_left

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/searching/BiSectLeft.kt
@@ -0,0 +1,34 @@
+package com.github.solairerove.algs4.leprosorium.searching
+
+fun main() {
+    val items = listOf(5, 6, 4, 3, 10, 9, 5, 6, 7)
+    print("items: $items \n")
+
+    val sorted = items.sorted()
+    print("items: $sorted \n")
+    print("trying to find 10, index: ${biSectLeft(sorted, 10)} \n") // 8
+}
+
+private fun biSectLeft(arr: List<Int>, target: Int, lo: Int = 0, hi: Int = arr.size): Int {
+    val n = arr.size
+    if (n == 0) return 0
+
+    var low = lo
+    var high = hi
+
+    if (target < arr[low]) return low
+    if (target > arr[high - 1]) return high
+
+    while (true) {
+        if (low + 1 == high) {
+            return if (target == arr[low]) low else (low + 1)
+        }
+
+        val mid = low + (high - low) / 2
+
+        when {
+            target <= arr[mid] -> high = mid
+            else -> low = mid
+        }
+    }
+}
