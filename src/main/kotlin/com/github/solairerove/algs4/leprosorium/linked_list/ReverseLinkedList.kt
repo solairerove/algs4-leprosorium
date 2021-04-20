69a18349343a7d23da14d5766bfa29f7d799bcf5
Mikita Kryvitski <solairerove@gmail.com>
2021-04-20 19:45:31 +0300

    feature: add reverse linked list

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/ReverseLinkedList.kt
@@ -0,0 +1,47 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+fun main() {
+    var head: ListNode? = ListNode(1)
+    val node2 = ListNode(2)
+    val node3 = ListNode(3)
+    val node4 = ListNode(4)
+    val node5 = ListNode(5)
+
+    head?.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    printLinkedList(head)
+    head = reverseList(head)
+    println()
+    printLinkedList(head)
+}
+
+private fun printLinkedList(head: ListNode?) {
+    var curr: ListNode? = head
+    while (curr != null) {
+        print("${curr.value} ")
+        val next = curr.next
+        curr = next
+    }
+}
+
+private class ListNode(var value: Int) {
+    var next: ListNode? = null
+}
+
+// O(n) time | O(1) space
+private fun reverseList(head: ListNode?): ListNode? {
+    var prev: ListNode? = null
+    var curr: ListNode? = head
+
+    while (curr != null) {
+        val next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    return prev
+}
