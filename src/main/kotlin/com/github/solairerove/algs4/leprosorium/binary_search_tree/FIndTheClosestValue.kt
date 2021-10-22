2bb54ddcebcdad993832a62ad3a938a1dd06d2ea
solairerove <solairerove@gmail.com>
2021-10-22 14:28:28 +0300

    feature: add valid example

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/binary_search_tree/FIndTheClosestValue.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/binary_search_tree/FIndTheClosestValue.kt
@@ -7,15 +7,15 @@ import kotlin.math.abs
         /   \
        7    21
       / \  / \
-     3  7 11 25
+     3  8 15 25
 */
 fun main() {
     val root = BST(14)
-    val rightNode1 = BST(11)
+    val rightNode1 = BST(15)
     val rightNode2 = BST(25)
     val rightNode3 = BST(21)
     val leftNode1 = BST(7)
-    val leftNode2 = BST(7)
+    val leftNode2 = BST(8)
     val leftNode3 = BST(3)
     root.left = leftNode1
     root.right = rightNode3
@@ -24,7 +24,7 @@ fun main() {
     leftNode1.left = leftNode3
     leftNode1.right = leftNode2
 
-    print(findTheClosestValueInBst(tree = root, target = 10))
+    print(findTheClosestValueInBst(tree = root, target = 16))
 }
 
 private open class BST(var value: Int) {
