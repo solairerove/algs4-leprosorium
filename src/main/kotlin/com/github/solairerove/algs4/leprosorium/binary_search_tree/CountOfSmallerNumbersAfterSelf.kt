71104b8140c0b1d340c67ce9af5eedbe0d2943bb
solairerove <solairerove@gmail.com>
2021-11-11 18:12:46 +0300

    feature: write bst solution for count of smaller numbers after self

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/binary_search_tree/CountOfSmallerNumbersAfterSelf.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/binary_search_tree/CountOfSmallerNumbersAfterSelf.kt
@@ -2,6 +2,7 @@ package com.github.solairerove.algs4.leprosorium.binary_search_tree
 
 fun main() {
     println(countSmallerNaiveSquared(intArrayOf(5, 2, 6, 1))) // [2, 1, 1, 0]
+    println(countSmallerBST(listOf(5, 2, 6, 1))) // [2, 1, 1, 0]
 }
 
 // O(nˆ2) time | O(n) space
@@ -15,4 +16,49 @@ fun countSmallerNaiveSquared(arr: IntArray): List<Int> {
         counts.add(counter)
     }
     return counts
+}
+
+// O(log(n)) time | O(n) space
+// O(nˆ2) time | O(n) space worst
+fun countSmallerBST(arr: List<Int>): List<Int> {
+    if (arr.isEmpty()) return listOf()
+
+    val counts = arr.toMutableList()
+    val bst = BST()
+    for (i in arr.size - 1 downTo 0) {
+        bst.put(arr[i], counts, i, 0)
+    }
+
+    return counts
+}
+
+class BST {
+    private var root: Node? = null
+
+    inner class Node(var value: Int) {
+        var right: Node? = null
+        var left: Node? = null
+        var segmentLeftCount = 0
+    }
+
+    fun put(value: Int, arr: MutableList<Int>, idx: Int, preSum: Int) {
+        root = put(root, value, arr, idx, preSum)
+    }
+
+    private fun put(root: Node?, value: Int, arr: MutableList<Int>, idx: Int, preSum: Int): Node {
+        var x = root
+
+        if (x == null) {
+            arr[idx] = preSum
+            x = Node(value = value)
+        } else if (value >= x.value) {
+            val newPreSum = preSum + x.segmentLeftCount + if (value > x.value) 1 else 0
+            x.right = put(x.right, value, arr, idx, newPreSum)
+        } else {
+            x.segmentLeftCount++
+            x.left = put(x.left, value, arr, idx, preSum)
+        }
+
+        return x
+    }
 }
