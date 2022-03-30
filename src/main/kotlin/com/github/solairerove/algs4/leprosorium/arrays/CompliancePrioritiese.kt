2cc9fa79670576b704b7241956b9712e14b28d4e
solairerove <solairerove@gmail.com>
2022-03-31 01:18:17 +0300

    feature: reassigned priorities

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/arrays/CompliancePrioritiese.kt
@@ -0,0 +1,19 @@
+package com.github.solairerove.algs4.leprosorium.arrays
+
+/*
+    https://leetcode.com/discuss/interview-question/1362915/amazon-hackerrank-question-priority-assignment
+ */
+fun main() {
+    reassignedPriorities(arrayOf(1, 4, 8, 4)).forEach { print("$it ") }// [1, 2, 3, 2]
+}
+
+// O(n) time | O(n) space
+private fun reassignedPriorities(priorities: Array<Int>): Array<Int> {
+    var priority = 1
+    val cnt = IntArray(100) { 0 }
+    priorities.forEach { cnt[it]++ }
+    for (i in cnt.indices) if (cnt[i] > 0) cnt[i] = priority++
+    for (i in priorities.indices) priorities[i] = cnt[priorities[i]]
+
+    return priorities
+}
