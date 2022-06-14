a6420ce379e3a147c0e3e2a113cb7e973df2a8c6
solairerove <solairerove@gmail.com>
2022-06-14 22:36:13 +0400

    feature: rewrite compliance priorities

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/arrays/CompliancePriorities.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/arrays/CompliancePriorities.kt
@@ -7,18 +7,17 @@ fun main() {
     reassignedPriorities(arrayOf(1, 4, 8, 4)).forEach { print("$it ") }// [1, 2, 3, 2]
 }
 
-// O(n) time | O(n) space
+// O(1) time | O(1) space
 private fun reassignedPriorities(priorities: Array<Int>): Array<Int> {
     var priority = 1
     val cnt = IntArray(100) { 0 }
-    priorities.forEach { cnt[it]++ }
-    for (i in cnt.indices) if (cnt[i] > 0) cnt[i] = priority++
-    for (i in priorities.indices) priorities[i] = cnt[priorities[i]]
-
+    priorities.forEach { cnt[it] = 1 }
+    cnt.forEachIndexed { idx, el -> if (el > 0) cnt[idx] = priority++ }
+    priorities.forEachIndexed { idx, el -> priorities[idx] = cnt[el] }
     return priorities
 }
 
-// O(n) time | O(n) space
+// O(n * log(n)) time | O(n) space
 private fun reassignedPrioritiesHS(priorities: Array<Int>): Array<Int> {
     var counter = 1
     val numberToMinimum = priorities.toSortedSet().associateWith { counter++ }
