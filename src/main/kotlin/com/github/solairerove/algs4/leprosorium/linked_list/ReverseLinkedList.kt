37b520dae53219ff992ede6d0be4e7c60e330df0
solairerove <solairerove@gmail.com>
2022-11-08 01:41:40 +0400

    feature: add doc on reverse linked list

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/ReverseLinkedList.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/ReverseLinkedList.kt
@@ -1,5 +1,12 @@
 package com.github.solairerove.algs4.leprosorium.linked_list
 
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ */
+
 // O(n) time | O(1) space
 fun reverseList(head: ListNode?): ListNode? {
     var prev: ListNode? = null
