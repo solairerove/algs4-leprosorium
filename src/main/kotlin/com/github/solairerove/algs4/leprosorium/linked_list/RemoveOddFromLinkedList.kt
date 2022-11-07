037e2eca53b93376ad3d5b2a763b10d3e850af9b
solairerove <solairerove@gmail.com>
2022-11-08 01:35:24 +0400

    feature: add doc on remove odd from linked list

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/RemoveOddFromLinkedList.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/RemoveOddFromLinkedList.kt
@@ -1,9 +1,22 @@
 package com.github.solairerove.algs4.leprosorium.linked_list
 
+/**
+ * Given a linked list of integers, return a similar linked list
+ * with all elements having odd values removed.
+ *
+ * For example, your linked list values are 1 -> 2 -> 3 -> 4.
+ * Your new linked list should be 2 -> 4
+ *
+ * Function description
+ * Complete the function deleteOdd below. The function
+ * must return a reference to the head ListNode of your
+ * linked list which has the odd values removed.
+ *
+ */
+
 // O(n) time | O(n) space
 fun deleteOdd(head: ListNode?): ListNode? {
     var curr: ListNode? = head
-
     var evenHeadNode: ListNode? = null
     var prevEvenNode: ListNode? = null
 
@@ -11,8 +24,7 @@ fun deleteOdd(head: ListNode?): ListNode? {
         if (curr.value % 2 == 0) {
             if (evenHeadNode == null) evenHeadNode = curr else prevEvenNode?.next = curr
             prevEvenNode = curr
-        }
-        else {
+        } else {
             prevEvenNode?.next = curr.next
         }
 
