6556546c3c1fd0df8da3d6177833d8ece4d1e31e
solairerove <solairerove@gmail.com>
2022-11-10 23:51:45 +0300

    feature: remove duplicated from sorted linked list tests

--- src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/RemoveDuplicatedFromSortedLinkedList.kt
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/RemoveDuplicatedFromSortedLinkedList.kt
@@ -1,27 +1,16 @@
 package com.github.solairerove.algs4.leprosorium.linked_list
 
-fun main() {
-    var head = ListNode(1)
-    val node2 = ListNode(1)
-    val node3 = ListNode(1)
-    val node4 = ListNode(2)
-    val node5 = ListNode(2)
-    val node6 = ListNode(3)
-
-    head.next = node2
-    node2.next = node3
-    node3.next = node4
-    node4.next = node5
-    node5.next = node6
-
-    printLinkedList(head)
-    head = deleteDuplicates(head)
-    println()
-    printLinkedList(head)
-}
+/**
+ * Given the head of a sorted linked list,
+ * delete all duplicates such that each element appears only once.
+ * Return the linked list sorted as well.
+ *
+ * Input: head = [1,1,2,3,3]
+ * Output: [1,2,3]
+ */
 
 // O(n) time | O(1) space
-private fun deleteDuplicates(head: ListNode): ListNode {
+fun deleteDuplicatesFromSorted(head: ListNode?): ListNode? {
     var curr: ListNode? = head
     while (curr != null) {
         var next = curr.next
