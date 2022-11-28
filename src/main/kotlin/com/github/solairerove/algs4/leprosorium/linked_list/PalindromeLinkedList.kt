e4ac09dc3a5ef8308daadf4a3d222e0d24ca5386
solairerove <solairerove@gmail.com>
2022-11-28 11:26:37 +0800

    feature: palindrome linked list and tests

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/PalindromeLinkedList.kt
@@ -0,0 +1,26 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+/**
+ * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+ *
+ * Input: head = [1 -> 2 -> 2 -> 1]
+ * Output: true
+ */
+
+// O(n) time | O(n) space
+fun isPalindromeArray(head: ListNode?): Boolean {
+    if (head == null) return true
+
+    var curr: ListNode? = head
+    val arr = mutableListOf<Int>()
+    while (curr != null) {
+        arr.add(curr.value)
+        curr = curr.next
+    }
+
+    var low = 0
+    var high = arr.size - 1
+    while (low < high) if (arr[low++] != arr[high--]) return false
+
+    return true
+}
---
+++ src/test/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/PalindromeLinkedListTest.kt
@@ -0,0 +1,29 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class PalindromeLinkedListTest : StringSpec({
+    "if the linked list is palindrome" {
+        forAll(
+            row(
+                ListNode(1).also {
+                    it.next = ListNode(2).also {
+                        it.next = ListNode(2).also {
+                            it.next = ListNode(1)
+                        }
+                    }
+                }, true
+            ),
+            row(
+                ListNode(1).also {
+                    it.next = ListNode(2)
+                }, false
+            )
+        ) { head, res ->
+            isPalindromeArray(head) shouldBe res
+        }
+    }
+})
