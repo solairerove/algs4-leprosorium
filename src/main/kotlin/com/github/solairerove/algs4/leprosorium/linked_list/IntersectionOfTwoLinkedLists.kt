6e53b59fcd039e3e49d9725a31802663d6e1fde0
solairerove <solairerove@gmail.com>
2022-12-01 20:19:47 +0800

    feature: intersection of two linked lists

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/IntersectionOfTwoLinkedLists.kt
@@ -0,0 +1,34 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+/**
+ * Given the heads of two singly linked-lists headA and headB,
+ * return the node at which the two lists intersect.
+ * If the two linked lists have no intersection at all, return null.
+ *
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * Output: Intersected at '8'
+ *
+ * Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+ * From the head of A, it reads as [4,1,8,4,5].
+ * From the head of B, it reads as [5,6,1,8,4,5].
+ * There are 2 nodes before the intersected node in A;
+ * There are 3 nodes before the intersected node in B.
+ *
+ * Note that the intersected node's value is not 1
+ * because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references.
+ * In other words, they point to two different locations in memory,
+ * while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+ */
+
+
+// O(n + m) time | O(1) space
+fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+    var curr1: ListNode? = headA
+    var curr2: ListNode? = headB
+    while (curr1 != curr2) {
+        curr1 = if (curr1 == null) headA else curr1.next
+        curr2 = if (curr2 == null) headB else curr2.next
+    }
+
+    return curr1
+}
---
+++ src/test/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/IntersectionOfTwoLinkedListsTest.kt
@@ -0,0 +1,68 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class IntersectionOfTwoLinkedListsTest : StringSpec({
+    val intersection1 = ListNode(8)
+    val intersection2 = ListNode(2)
+    "intersection of two linked lists" {
+        forAll(
+            row(
+                ListNode(4).also {
+                    it.next = ListNode(1).also {
+                        it.next = intersection1.also {
+                            it.next = ListNode(4).also {
+                                it.next = ListNode(5)
+                            }
+                        }
+                    }
+                },
+                ListNode(5).also {
+                    it.next = ListNode(6).also {
+                        it.next = ListNode(1).also {
+                            it.next = intersection1.also {
+                                it.next = ListNode(4).also {
+                                    it.next = ListNode(5)
+                                }
+                            }
+                        }
+                    }
+                },
+                intersection1
+            ),
+            row(
+                ListNode(1).also {
+                    it.next = ListNode(9).also {
+                        it.next = ListNode(1).also {
+                            it.next = intersection2.also {
+                                it.next = ListNode(4)
+                            }
+                        }
+                    }
+                },
+                ListNode(3).also {
+                    it.next = intersection2.also {
+                        it.next = ListNode(4)
+                    }
+                },
+                intersection2
+            ),
+            row(
+                ListNode(2).also {
+                    it.next = ListNode(6).also {
+                        it.next = ListNode(4)
+                    }
+                },
+                ListNode(1).also {
+                    it.next = ListNode(5)
+                },
+                null
+            ),
+        ) { head1, head2, res ->
+            getIntersectionNode(head1, head2) shouldBe res
+        }
+    }
+})
