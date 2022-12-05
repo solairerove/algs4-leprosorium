2811bb10be2ad8b269800c5d5ea31e45bd2cd007
solairerove <solairerove@gmail.com>
2022-12-05 12:20:28 +0800

    feature: merge k sorted lists naive approach

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/MergeKSortedLists.kt
@@ -0,0 +1,29 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+/**
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ *
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ *
+ * Input: lists = [[1 -> 4 -> 5], [1 -> 3 -> 4], [2 -> 6]]
+ * Output: [1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6]
+ * Explanation: The linked-lists are:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * merging them into one sorted list:
+ * 1->1->2->3->4->4->5->6
+ */
+
+// O(kN) time | O(1) space
+fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+    if (lists.isEmpty()) return null
+    if (lists.size == 1) return lists[0]
+
+    var head = mergeTwoLists(lists[0], lists[1])
+    IntRange(2, lists.size - 1).forEach { head = mergeTwoLists(head, lists[it]) }
+
+    return head
+}
---
+++ src/test/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/MergeKSortedListsTest.kt
@@ -0,0 +1,35 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class MergeKSortedListsTest : StringSpec({
+    "merge k sorted lists" {
+        forAll(
+            row(
+                arrayOf<ListNode?>(
+                    ListNode(1).also {
+                        it.next = ListNode(4).also {
+                            it.next = ListNode(5)
+                        }
+                    },
+                    ListNode(1).also {
+                        it.next = ListNode(3).also {
+                            it.next = ListNode(4)
+                        }
+                    },
+                    ListNode(2).also {
+                        it.next = ListNode(6)
+                    }
+                ), listOf(1, 1, 2, 3, 4, 4, 5, 6)
+            ),
+            row(
+                arrayOf(), null
+            )
+        ) { lists, res ->
+            mergeKLists(lists)?.toList() shouldBe res
+        }
+    }
+})
