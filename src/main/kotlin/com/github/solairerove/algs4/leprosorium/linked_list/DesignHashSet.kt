41371a2ece2a189c6c130fd636f04eab14bd6324
solairerove <solairerove@gmail.com>
2022-12-27 10:44:47 +0800

    feature: design hash set

---
+++ src/main/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/DesignHashSet.kt
@@ -0,0 +1,46 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+/**
+ * Design a HashSet without using any built-in hash table libraries.
+ *
+ * Implement MyHashSet class:
+ *
+ * void add(key) Inserts the value key into the HashSet.
+ * bool contains(key) Returns whether the value key exists in the HashSet or not.
+ * void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ */
+
+class MyHashSet {
+    private val maxLen = 1000000
+    private val set: Array<MutableList<Int>?> = arrayOfNulls(maxLen)
+
+    private fun getIndex(key: Int): Int = key % maxLen
+
+    private fun getPos(key: Int, index: Int): Int {
+        val bucket = set[index] ?: return -1
+
+        return bucket.indices.find { key == bucket[it] } ?: -1
+    }
+
+    fun add(key: Int) {
+        val index = getIndex(key)
+        val pos = getPos(key, index)
+        if (pos < 0) {
+            if (set[index] == null) set[index] = mutableListOf()
+            set[index]!!.add(key)
+        }
+    }
+
+    fun remove(key: Int) {
+        val index = getIndex(key)
+        val pos = getPos(key, index)
+        if (pos >= 0) set[index]!!.removeAt(pos)
+    }
+
+    fun contains(key: Int): Boolean {
+        val index = getIndex(key)
+        val pos = getPos(key, index)
+
+        return pos >= 0
+    }
+}
---
+++ src/test/kotlin/com/github/solairerove/algs4/leprosorium/linked_list/DesignHashSetTest.kt
@@ -0,0 +1,29 @@
+package com.github.solairerove.algs4.leprosorium.linked_list
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class DesignHashSetTest : FunSpec({
+
+    test("design hash set basic test") {
+        val hs = MyHashSet()
+        hs.add(1)
+        hs.add(2)
+        hs.contains(2) shouldBe true
+        hs.contains(3) shouldBe false
+        hs.remove(2)
+        hs.contains(2) shouldBe false
+    }
+
+    test("design hash set basic leetcode test") {
+        val myHashSet = MyHashSet()
+        myHashSet.add(1) // set = [1]
+        myHashSet.add(2) // set = [1, 2]
+        myHashSet.contains(1) shouldBe true // return True
+        myHashSet.contains(3) shouldBe false // return False, (not found)
+        myHashSet.add(2) // set = [1, 2]
+        myHashSet.contains(2) shouldBe true// return True
+        myHashSet.remove(2) // set = [1]
+        myHashSet.contains(2) shouldBe false// return False, (already removed)
+    }
+})
